917e6518dd30a5db8890d8537776851cf308cf21
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-04-20 16:04:11 -0400

    adding sort functionality

--- src/component/SearchBar/Search.jsx
+++ src/component/SearchBar/Search.jsx
@@ -5,14 +5,21 @@ import '../SearchBar/style.css';
 function SearchForm() {
 
 
+    function handleUserSearch(userName) {
+
+        // users.filter(userName)
+        console.log("This is user name have ben searched :", userName);
+    }
+
+
     return (
         <React.Fragment>
             <form>
                 <div className="form-row ">
-                      <div className="col-md-6 mb-3 ">
+                    <div className="col-md-6 mb-3 ">
                         <label>Search</label>
-                       <div className="input-group">
-                            <input type="text" className="form-control" name="search" onChange="" />
+                        <div className="input-group">
+                            <input type="text" className="form-control" name="search" onChange={(event) => { handleUserSearch(event.target.value) }} />
                             {/* <div className="input-group-prepend">
                                 <button className="btn btn-dark" >Search</button>
                             </div> */}
@@ -21,8 +28,6 @@ function SearchForm() {
 
                 </div>
             </form>
-
-
         </React.Fragment>
     )
 }
--- src/component/Table/Table.jsx
+++ src/component/Table/Table.jsx
@@ -1,77 +1,122 @@
 import React, { useState, useEffect } from "react";
+import '../Table/style.css'
 import API from '../../utils/API';
 
 
 
-function Table (){
-    
-    const [users, setUsers] = useState([
-        {
-        
-        picture:"",
-        name: "" ,
-        phone: "",
-        email: "",
-        dob: ""
-      }]);
-    
-      useEffect(() => {
-    
-        API.getUsers().then((res) => {
-    
-          const data = res.data.results;
-          const allUsers = data.map((user) => {
-    
-            return({
-              
-              picture: user.picture.medium,
-              name: user.name.first +" "+ user.name.last,
-              phone: user.phone,
-              email: user.email,
-              dob: user.dob.date.slice(0,10)
-            })
-          });
-          setUsers(allUsers);
-        });
-        
-    
-      },[]);
-    
-    
-    
-      console.log('this is state 0 info ',users[0].name,users[0].phone,users[0].picture,users[0].email,users[0].dob)
+function Table() {
 
-    return(
+  const [users, setUsers] = useState([
+    {
+      id: "",
+      picture: "",
+      name: "",
+      phone: "",
+      email: "",
+      dob: "",
+
+    }]);
+
+
+  const [orderState, setOrderState] = useState({ order: "asc" });
+
+  useEffect(() => {
+
+    API.getUsers().then((res) => {
+
+      const data = res.data.results;
+      const allUsers = data.map((user) => {
+
+        return ({
+          id: data.indexOf(user),
+          picture: user.picture.medium,
+          name: user.name.first + " " + user.name.last,
+          phone: user.phone,
+          email: user.email,
+          dob: user.dob.date.slice(0, 10),
+
+        })
+      });
+      console.log('this is all users on first update!!', allUsers)
+      setUsers(allUsers);
+    });
+
+
+  }, []);
+
+  const handleSort = (value) => {
+    console.log(`You click on ${value} !`);
+    let newOrder;
+
+    if (orderState.order === "asc") {
+
+      newOrder = users.sort(function (a, b) {
+
+        if (a[value] < b[value]) { return -1; }
+        if (a[value] > b[value]) { return 1; }
+        return 0;
+      })
+      setOrderState({ ...orderState, order: "dsc" });
+     
+
+    } else {
+      newOrder = users.sort(function (a, b) {
+
+        if (a[value] > b[value]) { return -1; }
+        if (a[value] < b[value]) { return 1; }
+        return 0;
+      })
+      setOrderState({ ...orderState, order: "asc" });
+      
+
+
+    }
+
+
+
+
+    console.log('this is our new order', newOrder)
+    console.log('this is the old users!!!', users)
+    setUsers([...users]);
+
+  }
+
+
+  console.log('Thos is the users state!!!', users)
+  return (
     <React.Fragment>
-        <table className="table table-striped table-dark">
-            <thead >
-                <tr>
-                <th scope="col" style={{padding:20}}>#</th>
-                <th scope="col" style={{padding:20}}>Image <span className="fas fa-sort ml-1"></span></th>
-                <th scope="col" style={{padding:20}}>Name <span className="fas fa-sort ml-1"></span></th>
-                <th scope="col" style={{padding:20}}>Phone <span className="fas fa-sort ml-1"></span></th>
-                <th scope="col" style={{padding:20}}>Email <span className="fas fa-sort ml-1"></span></th>
-                <th scope="col" style={{padding:20}}>DBO <span className="fas fa-sort ml-1"></span></th>
-                </tr>
-            </thead>
-            <tbody>
-                <React.Fragment>
-                {users.map((user,index)=>(
-                    <tr >
-                    <th scope="row">1</th>
-                    <td ><img src={user.picture} alt="img" className="rounded-circle" style={{ height: 70, width: 70 }} /></td>
-                    <td>{user.name}</td>
-                    <td>{user.phone}</td>
-                    <td>{user.email}</td>
-                    <td>{user.dob}</td>
-                    </tr>         
 
-                ))}        
-                </React.Fragment>
-            </tbody>
-        </table>
+
+      <table className="table table-striped table-dark">
+        <thead >
+          <tr>
+            <th scope="col" style={{ padding: 20 }}>#</th>
+            <th scope="col" style={{ padding: 20 }}>Image <span></span></th>
+            <th scope="col" style={{ padding: 20 }}>Name <span name="name" className="fas fa-sort ml-1" onClick={event => { handleSort("name") }}></span></th>
+            <th scope="col" style={{ padding: 20 }}>Phone <span name="phone" className="fas fa-sort ml-1" onClick={event => { handleSort("phone") }}></span></th>
+            <th scope="col" style={{ padding: 20 }}>Email <span name="email" className="fas fa-sort ml-1" onClick={event => { handleSort("email") }}></span></th>
+            <th scope="col" style={{ padding: 20 }}>DBO <span name="dbo" className="fas fa-sort ml-1" onClick={event => { handleSort("dbo") }}></span></th>
+          </tr>
+
+        </thead>
+        <tbody>
+          <React.Fragment>
+            {users.map((user, index) => (
+              <tr key={user.id} >
+                <th scope="row">{user.id + 1}</th>
+                <td ><img src={user.picture} alt="img" className="rounded-circle" style={{ height: 70, width: 70 }} /></td>
+                <td>{user.name}</td>
+                <td>{user.phone}</td>
+                <td>{user.email}</td>
+                <td>{user.dob}</td>
+              </tr>
+
+            ))}
+          </React.Fragment>
+        </tbody>
+      </table>
     </React.Fragment>
-    )
+  )
 }
 
 export default Table;
