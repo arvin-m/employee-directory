236f5786a38d058aa7189f7b8ab30692dd56e96d
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-04-23 00:15:19 -0400

    Updates

---
+++ precache-manifest.b62af02739f18265cbbfa8d1f86e8264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d61e34ff638f79d006fb26b6707aac1",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "be52edfd5c14835508f0",
+    "url": "/employee-directory/static/css/main.77cb1c13.chunk.css"
+  },
+  {
+    "revision": "cd2938fb8f84b5985176",
+    "url": "/employee-directory/static/js/2.e307b2fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.e307b2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be52edfd5c14835508f0",
+    "url": "/employee-directory/static/js/main.adb2cf96.chunk.js"
+  },
+  {
+    "revision": "7725ae6a5b49e8b1096a",
+    "url": "/employee-directory/static/js/runtime-main.3cbf02f2.js"
+  }
+]);
